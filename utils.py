ca45ab2fdf41138b7467249c3a6f05276dae1cf2
Xploeng <75328136+Xploeng@users.noreply.github.com>
2023-12-12 12:26:20 +0100

    bugfix

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def get_dataframe(type='csv'):
-    df = pd.read_csv("data/pv_weather_data_2019_to_2022."+ type, sep=" ")
+    return pd.read_csv("data/pv_weather_data_2019_to_2022."+ type, sep=" ")
