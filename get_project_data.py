9b34676a16e3c657eee09a218b4d460271961ce0
SarahHuewels <130989147+SarahHuewels@users.noreply.github.com>
2023-12-30 12:43:20 +0100

    clean up code

--- get_project_data.py
+++ get_project_data.py
@@ -17,10 +17,11 @@ def get_pv_data(PATH):
     return(df)
 
 # get&filter weather data
-def get_weather_data(PATH):
+def get_weather_data(PATH, station_id):
     df = pd.read_csv(PATH, sep=";")
     # extract years 2019-2022 from the dataset
-    df = df.iloc[23590:25051]
+    if station_id == '03379':
+        df = df.iloc[23590:25051]
     df.reset_index(inplace=True)
     # delete columns we don't need
     del df["index"]
@@ -41,26 +42,30 @@ def remove_colname_spaces(df):
     df.columns = column_names
 
 
-## get photovoltaik data
-# get pv data for year 2019
-df19 = get_pv_data("data/19-20.xlsx")
-# get pv data for year 2020, ATTENTION, 2020 was a schaltjahr with 366 days instead of 365
-df20 = get_pv_data("data/20-21.xlsx")
-# get pv data for year 2021
-df21 = get_pv_data("data/21-22.xlsx")
-# get pv data for year 2022
-df22 = get_pv_data("data/22-23.xlsx")
-# combine all years to one dataset 
-pv_df = pd.concat([df19, df20, df21, df22])
-pv_df.reset_index(inplace=True)
-del pv_df["index"]
+def combine_pv_and_weather_data():
+    ## get photovoltaik data
+    # get pv data for year 2019
+    df19 = get_pv_data("data/19-20.xlsx")
+    # get pv data for year 2020, ATTENTION, 2020 was a schaltjahr with 366 days instead of 365
+    df20 = get_pv_data("data/20-21.xlsx")
+    # get pv data for year 2021
+    df21 = get_pv_data("data/21-22.xlsx")
+    # get pv data for year 2022
+    df22 = get_pv_data("data/22-23.xlsx")
+    # combine all years to one dataset 
+    pv_df = pd.concat([df19, df20, df21, df22])
+    pv_df.reset_index(inplace=True)
+    del pv_df["index"]
 
-## Get weather data 
-weather_df = get_weather_data("data/produkt_klima_tag_19540601_20221231_03379.txt")
+    ## Get weather data 
+    weather_df = get_weather_data("data/produkt_klima_tag_19540601_20221231_03379.txt", station_id='03379')
 
-## Combine photovoltaik data and weather data to one dataset
-final_df = pd.concat([pv_df, weather_df], axis=1)
-final_df.rename(columns={"Gesamtanlage": "Gesamtanlage[kWh]"}, inplace=True)
+    ## Combine photovoltaik data and weather data to one dataset
+    final_df = pd.concat([pv_df, weather_df], axis=1)
+    final_df.rename(columns={"Gesamtanlage": "Gesamtanlage[kWh]"}, inplace=True)
+    return(final_df)
 
 ## write dataset into a csv file
-final_df.to_csv("data/pv_weather_data_2019_to_2022.csv", sep=" ", index=False, columns=final_df.columns)
+#final_df = combine_pv_and_weather_data()
+#print(final_df)
+#final_df.to_csv("data/pv_weather_data_2019_to_2022.csv", sep=" ", index=False, columns=final_df.columns)
