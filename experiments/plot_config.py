c5102a8384eaefbb5e25d3d0eaee9d3111bb722b
Xploeng <julius@harald-rau.de>
2024-01-24 11:59:23 +0100

    plot logic

---
+++ experiments/plot_config.py
@@ -0,0 +1,19 @@
+class config():
+    class labels():        
+        production = 'Electricity production [kWh]'
+        sunshine_label = 'Hours of sunshine'
+        
+    abbrevs={ 
+        'SDK' : 'sunshine',
+        'SNA' : 'Solar angle',
+        'TMK' : 'mean temp',
+        'TXK' : 'max temp',
+        'VPM' : 'vapor pressure',
+        'RSK' : 'rain',
+        'TNK' : 'min temp',
+        'SHK_TAG' : 'snow',
+        'UPM' : 'mean humidity',}
+    
+    class plotting_params():
+        dot_size = 10
+    
